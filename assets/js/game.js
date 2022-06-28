561c67eddb09280b375f9d280b82c16958891ef6
Simone Brin <simonebrin@gmail.com>
2022-06-28 11:21:45 -0400

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -80,7 +80,7 @@ var fight = function(enemyName) {
 }; // end of fight function
 
 // function to start a new game
-debugger;
+//debugger;
 var startGame = function() {
   // reset player stats
   playerHealth = 100;
@@ -100,10 +100,33 @@ var startGame = function() {
       window.alert("You have lost your robot in battle! Game Over!");
       break;
     }
+    // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+  endGame();
+};
   }
   // play again
   //startGame();
-};
+  // function to end the entire game
+  var endGame = function() {
+    // if player is still alive, player wins!
+    if (playerHealth > 0) {
+      window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    } 
+    else {
+      window.alert("You've lost your robot in battle.");
+    }
+    // ask player if they'd like to play again
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
+} 
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+}
+  }
+  
 
 // start the game when the page loads
 startGame();
